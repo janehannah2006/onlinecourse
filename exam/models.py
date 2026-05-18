3ff7ff7cb34cd6108ddf28bb5e3b72c189bae55b
janehannah2006 <727824tuam018@skct.edu.in>
2026-05-18 17:26:14 +0530

    Create models.py

---
+++ exam/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    is_correct = models.BooleanField(default=False)
+
+class Submission(models.Model):
+    student_name = models.CharField(max_length=100)
+    score = models.IntegerField(default=0)
